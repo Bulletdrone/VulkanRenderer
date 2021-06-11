27b1ae39b37c09f40f9611a069440f0f2b7eb6d8
Sam <samboots123@gmail.com>
2021-06-11 15:07:48 +0200

    Renderpass ColorAttachment to LOAD_OP_DONT_CARE
    Change:
    Renderer.cpp, CreateRenderPass(), t_ColorAttachment.loadOp is now changes so that all the objects can be rendered.
    
    main.cpp, now creates the 3 other objects as well since this now works.

--- VulkanRenderer/VulkanRendererWindows/main.cpp
+++ VulkanRenderer/VulkanRendererWindows/main.cpp
@@ -19,9 +19,9 @@ int main()
     Transform* t_Transform4 = new Transform(glm::vec3(0.0f, 0.7f, 0.4), 1);
 
     m_ObjectManager->CreateShape(ShapeType::Rectangle, t_Transform1);
-    //m_ObjectManager->CreateShape(ShapeType::Triangle, t_Transform2);
-    //m_ObjectManager->CreateShape(ShapeType::Triangle, t_Transform3);
-    //m_ObjectManager->CreateShape(ShapeType::Rectangle, t_Transform4);
+    m_ObjectManager->CreateShape(ShapeType::Triangle, t_Transform2);
+    m_ObjectManager->CreateShape(ShapeType::Triangle, t_Transform3);
+    m_ObjectManager->CreateShape(ShapeType::Rectangle, t_Transform4);
     
 
     m_ObjectManager->SetupStartObjects();
